c03459af69ab4788b33630223f300b90e27573d5
nxfuci <nxflores@uci.edu>
2025-01-12 13:01:53 -0800

    UI Complete

--- Assets/Scripts/PlantCountUI.cs
+++ Assets/Scripts/PlantCountUI.cs
@@ -8,6 +8,8 @@ public class PlantCountUI : MonoBehaviour
 
     public void UpdateSeeds (int seedsLeft, int seedsPlanted)
     {
-        
+        _plantedText.text = "Seeds Planted: " + seedsPlanted.ToString();
+
+        _remainingText.text = "Remaining Seeds: " + seedsLeft.ToString();
     }
 }
